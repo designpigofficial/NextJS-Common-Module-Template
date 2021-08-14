d4c33a65fb53b5fc796a0463eca8c1e4c5970eb8
Xxshark888xX <mutu.adi.marian@gmail.com>
2021-08-14 12:14:37 +0300

    static files should be imported

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -2,15 +2,16 @@ import Page from '@fpage';
 import Link from '@link';
 import Image from '@image';
 
-export default function Page2() {
+import doggoImg from '@passets/loading_image_example.jpg';
 
+export default function Page2() {
   return (
     <Page headTags={['<title>/Page2</title>']}>
       <h1>Page2</h1>
       <br />
       <h4>The doggo is rendered with the custom Image component which shows a customizable loader element while the image is loading</h4>
       <br />
-      <Image src={'/loading_image_example.jpg'} width={1020} height={1120} />
+      <Image src={doggoImg} width={1020} height={1120} />
       <br />
       <Link href='/'>Go to Home</Link>
     </Page>
